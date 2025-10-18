e23b2b2e0736ad59289063c21037fa6cfc6cab0d
PedroXits <pedroo.asn@gmail.com>
2025-10-18 10:37:50 -0300

    primeiro commit

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Rodando na porta ${port}`));
