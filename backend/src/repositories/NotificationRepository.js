ce6db404a0391d9cc624411933243b287f216a31
Flavio Fleck <flaviofleck55@gmail.com>
2025-11-09 10:51:42 -0300

    Criação do NotificationService e dos métodos createNotification e deleteNotification

--- backend/src/repositories/NotificationRepository.js
+++ backend/src/repositories/NotificationRepository.js
@@ -1,14 +1,14 @@
-export default class NotificartionRepository {
+export default class NotificationRepository {
     constructor(connection) {
         this.connection = connection
     }
 
-    add({senderId, receiverId, content, type, messageId, likeId, postId, followId}) {
+    async add({senderId, receiverId, content, type, messageId, likeId, postId, followId}) {
         const query = `
             INSERT INTO notifications(sender_user_id, receiver_user_id, content, type, message_id, like_id, post_id, follow_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         `
 
-        const [info] = this.connection.query(query, [
+        const [info] = await this.connection.query(query, [
             senderId, 
             receiverId, 
             content, 
@@ -22,9 +22,9 @@ export default class NotificartionRepository {
         return info.insertId
     }
     
-    delete({id}) {
+    async delete({id}) {
         const query = "DELETE FROM notifications WHERE id = ?"
-        const [info] = this.connection.query(query, [id])
+        const [info] = await this.connection.query(query, [id])
         return info.affectedRows
     }
 }
---
+++ backend/src/services/NotificationService.js
@@ -0,0 +1,19 @@
+import Notification from "../models/Notification";
+import NotificationRepository from "../repositories/NotificationRepository.js";
+
+export default class NotificationService {
+    constructor(connection) {
+        this.notificationRepository = new NotificationRepository(connection)
+    }
+
+    async createNotification(payload) {
+        const notification = new Notification(payload)
+        const result = await this.notificationRepository.add(notification)
+        return result
+    }
+
+    async deleteNotification(payload) {
+        const result = await this.notificationRepository.delete(payload)
+        return result
+    }
+}
