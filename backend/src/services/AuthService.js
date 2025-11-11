a7d66474cdff6b18bac427060b319e38b3436a60
PedroXits <pedroo.asn@gmail.com>
2025-11-11 19:05:33 -0300

    Criação das funções de Login e Register no AuthService

---
+++ backend/src/services/AuthService.js
@@ -0,0 +1,60 @@
+import User from "../models/User.js";
+import { generateToken } from "../utils/jwt.js";
+import bcrypt from "bcrypt";
+import UserRepository from "../repositories/UserRepository.js";
+
+export default class AuthService {
+    constructor(connection) {
+        this.userRepository = new UserRepository(connection);
+    }
+    
+    register = async (payload) => {
+        const {name, lastname, email, password, birthday} = payload;
+        const existingUser = await this.userRepository.getByEmail({ email });
+        if (existingUser) {
+            throw new Error("Email já está em uso");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User ({
+            name,
+            lastname,
+            email,
+            password: hashedPassword,
+            birthday
+        });
+
+        const insertId = await this.userRepository.add(user);
+        const token = generateToken({
+            id: insertId,
+            name,
+            lastname,
+            email,
+            birthday
+        });
+
+        return { id: insertId, token };
+    };
+
+    login = async (email, password) =>{
+        const user = await this.userRepository.getByEmail({ email });
+        if (!user) {
+            throw new Error("Usuário incorreto ou não encontrado.");
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch) {
+            throw new Error("Senha incorreta");
+        }
+
+        const token = generateToken({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            lastname: user.lastname
+        });
+
+        return { token };
+    };
+}
