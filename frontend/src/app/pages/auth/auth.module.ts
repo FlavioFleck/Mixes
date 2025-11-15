1c0ca2d659d33c26ebe276680e6981efc3e96533
PedroXits <pedroo.asn@gmail.com>
2025-11-15 17:53:46 -0300

    Criação da pasta Auth + Login components + Register components

---
+++ frontend/src/app/pages/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import './auth-layout.css'; // 🔥 importa o CSS globalmente só pra este módulo
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule]
+})
+export class AuthModule {}
