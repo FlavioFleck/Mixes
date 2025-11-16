fec1a1278efb188cb83d37eb70578a26342312eb
Flavio Fleck <flaviofleck55@gmail.com>
2025-11-16 10:17:08 -0300

    adição da rota de post ou router

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -2,10 +2,12 @@ import express from 'express';
 import userRouter from './userRoutes.js'
 import authRoutes from './authRoutes.js'
 import profileRoutes from "./profileRoutes.js"
+import postRoutes from './postRoutes.js'
 
 const router = express.Router();
 router.use('/user', userRouter)
 router.use('/auth', authRoutes)
 router.use('/profile', profileRoutes)
+router.use('/post', postRoutes)
 
 export default router;
