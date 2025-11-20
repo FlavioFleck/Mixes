86e280650d2d18ea2dfda579d12fd7a9037b13b7
PedroXits <pedroo.asn@gmail.com>
2025-11-20 14:05:24 -0300

    Criação da função createProfile dentro do create-profile.ts

--- frontend/src/app/pages/auth/create-profile/create-profile.ts
+++ frontend/src/app/pages/auth/create-profile/create-profile.ts
@@ -1,14 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ProfileService } from '../../../services/profile.service';
 
 @Component({
   selector: 'app-create-profile',
+  standalone: true,
   imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './create-profile.html',
   styleUrl: './create-profile.css',
 })
 export class CreateProfile {
+  username = '';
+  bio = '';
+  image: File | null = null;
 
+  constructor(private profileService: ProfileService, private router: Router){
+
+  }
+
+  onFileChange(event: any) {
+    this.image = event.target.files[0];
+  }
+
+  createProfile() {
+    const userId = localStorage.getItem('userId');
+
+    const formData = new FormData();
+    formData.append('username', this.username);
+    formData.append('bio', this.bio);
+    formData.append('userId', userId || '');
+
+    if(this.image){
+      formData.append('image', this.image);
+    }
+
+    this.profileService.createProfile(formData).subscribe({
+      next: (res) => {
+        alert('Perfil criado com sucesso');
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        alert(err.error.error || 'Erro ao criar perfil');
+      }
+    });
+  }
 }
