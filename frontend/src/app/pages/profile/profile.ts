5aa597b888c3b369c993827b1b7ee0d1e463727c
wkarina <karii46y@gmail.com>
2025-11-20 18:10:05 -0300

    adição do botão edit profile dentro do /profile

--- frontend/src/app/pages/profile/profile.ts
+++ frontend/src/app/pages/profile/profile.ts
@@ -10,5 +10,20 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './profile.css',
 })
 export class Profile {
-  
+  isLoggedIn = true; // simulação de usuario logado (se mudar p false o botão some)
+  isEditModalOpen = false; // controle do modal
+
+  openModal() {
+    this.isEditModalOpen = true;
+  }
+
+  closeModal() {
+    this.isEditModalOpen = false;
+  }
+
+  saveProfile() {
+    // lógica de salvar no backend entra aqui
+    console.log('Perfil salvo!');
+    this.closeModal();
+  }
 }
