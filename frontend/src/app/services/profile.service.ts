af1f1c6149c4617a8f45a4e00af19ef08c5c2f12
PedroXits <pedroo.asn@gmail.com>
2025-11-20 13:53:33 -0300

    Criação do profile service no frontend

---
+++ frontend/src/app/services/profile.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  private API_URL = 'http://localhost:5010/profile';
+
+  constructor(private http: HttpClient) {}
+
+  createProfile(data: any) {
+    return this.http.post(`${this.API_URL}/create`, data);
+  }
+
+  updateProfile(data: any) {
+    return this.http.put(`${this.API_URL}/update`, data);
+  }
+
+  deleteProfile() {
+    return this.http.delete(`${this.API_URL}/delete`);
+  }
+
+  getProfileByUsername(username: string) {
+    return this.http.get(`${this.API_URL}/${username}`);
+  }
+}
