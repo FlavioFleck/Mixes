2e1c0a828e2c9c2846f751eca6b6751936cfc956
PedroXits <pedroo.asn@gmail.com>
2025-11-20 15:10:59 -0300

    Adição do multer + middleware (validação) no profileRoutes

--- backend/src/routes/profileRoutes.js
+++ backend/src/routes/profileRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
+import multer from "multer";
 import { connection } from '../connection.js'
 import ProfileController from "../controllers/ProfileController.js"
 import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
+const upload = multer({ dest: "uploads/profile/" });
 const profileController = new ProfileController(connection)
 
-router.post("/profile/create", authMiddleware, profileController.createProfile);
+router.post("/profile/create", upload.single("photo"), authMiddleware, profileController.createProfile);
 router.post("/delete", profileController.deleteProfile)
 router.get("/get", profileController.viewProfile)
 router.get("get/:id", profileController.viewProfiles)
